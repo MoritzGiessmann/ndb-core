472f49c07ace1f011384cb300029720cecc468db
Simon <therealslimv@yahoo.de>
2019-03-28 21:03:44 +0100

    added more router testing modules

--- src/app/ui-helper/entity-subrecord/entity-subrecord.component.spec.ts
+++ src/app/ui-helper/entity-subrecord/entity-subrecord.component.spec.ts
@@ -14,6 +14,7 @@ import {EntityMapperService} from '../../entity/entity-mapper.service';
 import {ConfirmationDialogService} from '../confirmation-dialog/confirmation-dialog.service';
 import {Database} from '../../database/database';
 import {MockDatabaseManagerService} from '../../database/mock-database-manager.service';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('EntitySubrecordComponent', () => {
   let component: EntitySubrecordComponent;
@@ -22,8 +23,16 @@ describe('EntitySubrecordComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ EntitySubrecordComponent ],
-      imports: [MatTableModule, MatFormFieldModule, MatIconModule,
-        MatSnackBarModule, MatSelectModule, MatDialogModule, MatAutocompleteModule],
+      imports: [
+        MatTableModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatSnackBarModule,
+        MatSelectModule,
+        MatDialogModule,
+        MatAutocompleteModule,
+        RouterTestingModule.withRoutes([])
+      ],
       providers: [
         { provide: Database, useValue: new MockDatabaseManagerService().getDatabase() },
         ConfirmationDialogService,
